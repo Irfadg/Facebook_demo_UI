a6c52180f820edc5e586506116cf06ee462174ae
Irfad <irfadg61@gmail.com>
2021-11-19 14:52:53 +0530

    basic facebook login demo

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(Myapp())
+  runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
-  const Myapp({ Key? key }) : super(key: key);
+  const Myapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,20 +14,68 @@ class Myapp extends StatelessWidget {
       theme: ThemeData(
         backgroundColor: Colors.indigo,
       ),
-      home:Loginpage() ,
+      home: const Loginpage(),
     );
   }
-
 }
 
 class Loginpage extends StatelessWidget {
-   Loginpage({ Key? key }) : super(key: key);
+  const Loginpage({Key? key}) : super(key: key);
 
   @override
-  Widget ldContext context) {
-    return Scaffold (
-      
-      
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+          child: Center(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                
+                // decoration: BoxDecoration(
+                //borderRadius: BorderRadius.circular(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.mic)),
+                    const Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: Column(
+                  
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Text(
+                      "LOGIN",
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                    Text(
+                      "sign-up",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      ),
+          )),
     );
   }
 }
